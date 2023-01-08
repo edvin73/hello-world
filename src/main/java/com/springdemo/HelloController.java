dd3cfb2c1759d89deed507ff07cf8ba4284931c4
Edvin <e.tuzlakovic@gmail.com>
2023-01-08 18:58:05 +0100

    HelloController change format endpoint now

--- src/main/java/com/springdemo/HelloController.java
+++ src/main/java/com/springdemo/HelloController.java
@@ -22,14 +22,23 @@ public class HelloController {
 		
 		
 		int year = LocalDateTime.now().getYear();
+		
 		int month = LocalDateTime.now().getMonth().getValue();
+		String sMonth = String.format("%02d" , month);
+		
 		int day = LocalDateTime.now().getDayOfMonth();
+		String sDay = String.format("%02d" , day);
 		
 		int hour = LocalDateTime.now().getHour();
-		int minute = LocalDateTime.now().getMinute();
-		int sec = LocalDateTime.now().getSecond();
+		String sHour = String.format("%02d" , hour);
 		
-		String val = day + "/"+ month + "/" + year + " " +  hour + ":" + minute + ":" + sec;
+		int minute = LocalDateTime.now().getMinute();
+		String sMin = String.format("%02d" , minute);
+		
+		int sec = LocalDateTime.now().getSecond();
+		String sSec = String.format("%02d" , sec);
+		
+		String val = "Date: " + sDay + "/"+ sMonth + "/" + year + " " +  sHour + ":" + sMin + ":" + sSec;
 		
 		return val;
 	}
