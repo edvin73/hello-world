eb8af592513a72e87261d97784dca4dc05035333
Edvin <e.tuzlakovic@gmail.com>
2023-01-08 01:56:17 +0100

    commit Version 1

---
+++ src/main/java/com/springdemo/HelloController.java
@@ -0,0 +1,17 @@
+package com.springdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+	
+	@GetMapping(value = "/hello/{name}")
+	public String hello(@PathVariable(name = "name") String name) {
+		return "Hello " + name;
+	}
+
+}
